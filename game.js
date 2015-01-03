eff5f7cd23439103a7c22b3aa8bc0ecb3e494e9c
Jett <whackashoe@gmail.com>
2015-01-02 22:13:48 -0600

    initial commit

---
+++ game.js
@@ -0,0 +1,91 @@
+(function() {
+    var canvas    = document.getElementById("canvas");
+    var ctx       = canvas.getContext("2d");
+    canvas.width  = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+
+
+
+    var game = {
+        world: new Box2D.b2World(new Box2D.b2Vec2(0, -10), false),
+        draw_offset: { x: 0, y: 0 }, /* translation of game world render */
+        PTM: 32, /* pixels to meters */
+        asteroids: [],
+        balls    : [],
+        coins    : [],
+
+        init: function() {
+            {
+                this.balls.push(this.create_ball());
+            }
+        },
+
+        init_debug_draw: function() {
+            //this.world.SetDebugDraw();
+            //do we want a debug draw?
+            //maybe just go straight for rendering
+        },
+        
+        create_ball: function() {
+            var bd = new Box2D.b2BodyDef();
+            bd.set_type(Box2D.b2_dynamicBody);
+            bd.set_position( new Box2D.b2Vec2(0, 1) );
+
+
+            var circleShape = new Box2D.b2CircleShape();
+            circleShape.set_m_radius(0.4);
+
+            var fd = new Box2D.b2FixtureDef();
+            fd.set_shape(circleShape);
+            fd.set_density(1.0);
+            fd.set_friction(0.9);
+
+            var body = this.world.CreateBody(bd);
+            body.CreateFixture(fd);
+
+            return {
+                body: body,
+            };
+        },
+
+        create_asteroid: function() {
+            return {
+
+            };
+        },
+        
+        create_coin: function() {
+            return {
+
+            };
+        },
+
+        step: function() {
+            this.world.Step(1 / 60, 10, 10);
+            this.world.ClearForces();
+            console.log(this.balls[0].body.GetPosition().get_y());
+        },
+
+        render: function() {
+            ctx.fillStyle = 'rgb(0,0,0)';
+            ctx.fillRect( 0, 0, canvas.width, canvas.height );
+            
+            ctx.save();            
+                ctx.translate(this.draw_offset.x, this.draw_offset.y);
+                ctx.scale(1, -1);                
+                ctx.scale(this.PTM, this.PTM);
+                ctx.lineWidth /= this.PTM;                
+                ctx.fillStyle = 'rgb(255,255,0)';
+                this.world.DrawDebugData();
+                
+            ctx.restore();
+        }
+    };
+
+    game.init_debug_draw();
+    game.init();
+    setInterval(function() {
+        game.step();
+        game.render();
+    }, 1000.0 / 60);
+})();
