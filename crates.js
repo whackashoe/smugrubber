88a370897c01400b6b38f5c88b468df57cdf3561
Jett <whackashoe@gmail.com>
2016-02-25 19:19:55 -0600

    move data out to files

---
+++ crates.js
@@ -0,0 +1,12 @@
+var crates = [
+    {
+        name: "health pack",    
+        width:        0.5,
+        height:       0.5,
+        density:      1.0,
+        friction:     1.0,
+        restitution:  0.2,
+        color:        "rgb(180, 20, 90)"
+    }
+];
+
---
+++ particles.js
@@ -0,0 +1,8 @@
+var particles = [
+    {
+        color: "rgb(50, 50, 50)",
+        radius: 0.2,
+        lifetime: 100
+    }
+];
+
