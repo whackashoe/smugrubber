a7c5436673c9dc36f38cf2423636302e9abc0122
Jett <whackashoe@gmail.com>
2016-03-07 15:08:07 -0600

    ninja types

---
+++ js/game/objects/ninjas.js
@@ -0,0 +1,25 @@
+var m_ninjas = [
+    {
+        max_damage: 20,
+        stock: 2,
+        move: {
+            strength: 28,
+            max_speed: 15
+        },
+        jetpack: {
+            strength:     1.2,
+            max_speed:    15,
+            max_ammo:     100,
+            reload_rate:  0.32
+        },
+        body: {
+            radius: 0.75,
+            density: 1.0,
+            friction: 0.1,
+            restitution: 0.02
+        },
+        toss: {
+            force_mult: 15
+        }
+    }
+];
--- js/game/objects/settings.js
+++ js/game/objects/settings.js
@@ -31,29 +31,6 @@ var settings = {
             b: 53
         }
     },
-    ninja: {
-        max_damage: 20,
-        stock: 2,
-        move: {
-            strength: 28,
-            max_speed: 15
-        },
-        jetpack: {
-            strength:     1.2,
-            max_speed:    15,
-            max_ammo:     100,
-            reload_rate:  0.32
-        },
-        body: {
-            radius: 0.75,
-            density: 1.0,
-            friction: 0.1,
-            restitution: 0.02
-        },
-        toss: {
-            force_mult: 15
-        }
-    },
     crates: {
         health_restore: 5.0,
         jet_fuel:       1000
